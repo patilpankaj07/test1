12878afa6f7e2607e7a94b645a328fa75850cd17
patilpankaj07 <108424777+patilpankaj07@users.noreply.github.com>
2024-05-02 04:23:38 +0530

    Update server.js

--- static/server.js
+++ static/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));.
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(PORT, () => {
